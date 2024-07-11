cfedafce3702ea0a68aa6d19d6a9bda89e36545d
Martina Emerson <wangdongovo@gmail.com>
2024-07-11 20:29:00 +0800

    fix: /

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -1,6 +1,1 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' }).then((registration) => {
-    console.log('服务工作线程注册成功，范围:', registration.scope)
-  })
-  .catch((error) => {
-    console.error('服务工作线程注册失败:', error)
-  })
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
