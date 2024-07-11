9b6dc88895fa5579bbe3afe2699f54877eb37584
Martina Emerson <wangdongovo@gmail.com>
2024-07-11 10:47:54 +0800

    fix: /

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,3 +1,9 @@
+/**
+ *一旦用户选择授权，这个授权结果对通知 API 和推送 API 两者都有效。
+ * 用户的授权的结果有三种，default（默认）、granted（允许）或者 denied（拒绝），
+ * 当用户没有做出选择的时候，授权结果会返回 default，另外两种结果分别在用户选择了
+ * 允许或者拒绝的时候返回。*/
+
 export async function requestNotificationPermission() {
   if ('Notification' in window) {
     const permission = await Notification.requestPermission()
