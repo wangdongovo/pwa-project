b54fa6c3adc4ff9c4cf5827351c76537cf004d48
Martina Emerson <wangdongovo@gmail.com>
2024-07-11 19:28:57 +0800

    fix: /

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { registerSW } from 'virtual:pwa-register'
 import { requestNotificationPermission } from '@/utils/notifications'
 
 if ('serviceWorker' in navigator) {
@@ -28,14 +27,3 @@ app.use(router)
 app.mount('#app')
 
 requestNotificationPermission()
-
-const updateSW = registerSW({
-  onNeedRefresh() {
-    // 显示一个通知给用户，让他们手动刷新
-    console.log('新的内容可用，请刷新页面。')
-  },
-  onOfflineReady() {
-    // 应用准备离线使用
-    console.log('应用现在可供离线使用。')
-  }
-})
