edd0cb615b5726d7226c248d8b36e2cba70f4230
Martina Emerson <wangdongovo@gmail.com>
2024-07-11 20:19:48 +0800

    fix: /

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -1,1 +1,6 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' }).then((registration) => {
+    console.log('服务工作线程注册成功，范围:', registration.scope)
+  })
+  .catch((error) => {
+    console.error('服务工作线程注册失败:', error)
+  })
--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,33 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { registerSW } from 'virtual:pwa-register'
 import { requestNotificationPermission } from '@/utils/notifications'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
-    .then((registration) => {
-      console.log('服务工作线程注册成功，范围:', registration.scope)
-    })
-    .catch((error) => {
-      console.error('服务工作线程注册失败:', error)
-    })
-}
+
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // 显示一个通知给用户，让他们手动刷新
+    console.log('新的内容可用，请刷新页面。')
+  },
+  onOfflineReady() {
+    // 应用准备离线使用
+    console.log('应用现在可供离线使用。')
+  }
+})
+
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+//     .then((registration) => {
+//       console.log('服务工作线程注册成功，范围:', registration.scope)
+//     })
+//     .catch((error) => {
+//       console.error('服务工作线程注册失败:', error)
+//     })
+// }
 
 const app = createApp(App)
 
